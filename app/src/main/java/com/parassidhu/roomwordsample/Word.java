8f2764238afef4f29955e637e94173a198170f1c
Paras Sidhu <sidhuparas1234@gmail.com>
2018-07-04 16:57:40 +0530

    Initial Commit

---
+++ app/src/main/java/com/parassidhu/roomwordsample/Word.java
@@ -0,0 +1,23 @@
+package com.parassidhu.roomwordsample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(String mWord) {
+        this.mWord = mWord;
+    }
+
+    public String getWord() {
+        return mWord;
+    }
+}
